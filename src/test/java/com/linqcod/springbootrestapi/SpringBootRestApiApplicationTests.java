42e576462a91a215d6941a9e675c55c6a5962661
linqcod <linqcod@yandex.ru>
2021-10-14 15:17:11 +0300

    Initial commit

---
+++ src/test/java/com/linqcod/springbootrestapi/SpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linqcod.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
